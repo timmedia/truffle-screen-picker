645a664b418e9534191febfeb1139c9fafa95f74
timmedia <contact@tim-media.com>
2023-03-26 15:36:53 +0200

    fix: video scaling

--- src/App.tsx
+++ src/App.tsx
@@ -41,28 +41,41 @@ function App() {
     console.log(window.parent.document);
   };
 
-  const onIframeLoad = () => {
-    embed.setStyles({
-      minHeight: "480px",
-    });
+  const onIframeLoad = (e: any) => {
+    console.log(e);
+    // embed.setStyles({
+    //   minHeight: "480px",
+    // });
+  };
+
+  const handleClick = (event: any) => {
+    console.log(event.clientX, event.clientY);
   };
 
   return (
     <div className="app">
-      <main>
-        <div className="video-container">
-          <iframe
-            src="https://yewtu.be/embed/DfJcWZoguQY?volume=0&controls=0"
-            className="video-frame"
-            onLoad={onIframeLoad}
-            ref={iframeRef}
-          ></iframe>
-        </div>
-        <div className="controls">
-          <h1>Select your position</h1>
-          <button onClick={setSize}>Toggle Size</button>
-          <button onClick={setBorder}>Toggle Border</button>
-          <button onClick={setPosition}>Set position</button>
+      <main onClick={handleClick}>
+        <iframe
+          src="https://yewtu.be/embed/DfJcWZoguQY?volume=0&controls=0"
+          className="video-frame"
+          onLoad={onIframeLoad}
+          ref={iframeRef}
+        ></iframe>
+        <div className="overlay">
+          <div className="overlay-text">
+            <svg viewBox="0 0 140 80" xmlns="http://www.w3.org/2000/svg">
+              <text
+                text-anchor="middle"
+                alignment-baseline="central"
+                x="70"
+                y="45"
+                color="white"
+              >
+                Click to submit
+              </text>
+            </svg>
+          </div>
+          <div className="overlay-color"></div>
         </div>
       </main>
     </div>
