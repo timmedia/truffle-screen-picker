023d050fa6491a20178e4936d9ae74c3e0f50854
timmedia <contact@tim-media.com>
2023-04-04 05:27:16 +0200

    feat: verify user is admin in poll management

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,7 @@ import * as admin from "firebase-admin";
 import { verify } from "jsonwebtoken";
 import { uuid } from "uuidv4";
 import { initializeApp } from "firebase-admin/app";
+import { request, gql } from "graphql-request";
 
 initializeApp();
 const db = admin.database();
@@ -59,11 +60,66 @@ function verifyUserId(accessToken: string, userId: string) {
   }
 }
 
+/**
+ * Checks whether user has the admin role for the specified org.
+ * @param {string} accessToken
+ * @param {string} orgId
+ * @return {Promise<boolean>}
+ */
+async function verifyUserAdmin(accessToken: string, orgId: string) {
+  const endpoint = "https://mycelium.staging.bio/graphql";
+  const query = gql`
+    query RoleConnectionQuery(
+      $input: RoleConnectionInput
+      $first: Int
+      $after: String
+      $last: Int
+      $before: String
+    ) {
+      roleConnection(
+        input: $input
+        first: $first
+        after: $after
+        last: $last
+        before: $before
+      ) {
+        totalCount
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+        nodes {
+          id
+          orgId
+          name
+          slug
+          rank
+          isSuperAdmin
+        }
+      }
+    }
+  `;
+  const data = (await request({
+    url: endpoint,
+    document: query,
+    variables: { input: { orgId } },
+    requestHeaders: { "x-access-token": accessToken },
+  })) as any;
+  return data.roleConnection.nodes.some(
+    (node: any) => node.name === "Admin" && node.orgId === orgId
+  );
+}
+
 export const submitVote = functions.https.onCall(
   async (data: SubmitVoteData) => {
     const { accessToken, userId, relX, relY, pollId } = data;
     const isAllowed = verifyUserId(accessToken, userId);
-    if (!isAllowed) return { success: false };
+    if (!isAllowed) {
+      return {
+        success: false,
+        error: new Error("Access token does not match user id."),
+      };
+    }
     await db.ref(`polls/${pollId}/${userId}`).set([relX, relY]);
     return { success: true };
   }
@@ -73,9 +129,16 @@ export const createPoll = functions.https.onCall(
   async (data: CreatePollData) => {
     const { accessToken, userId, orgId } = data;
     const isAllowed = verifyUserId(accessToken, userId);
-    if (!isAllowed) return { success: false };
-    // TODO: check whether user is authorized to create a poll
-    // must somehow determine whether user is admin of channel of interest
+    if (!isAllowed) {
+      return {
+        success: false,
+        error: new Error("Access token does not match user id."),
+      };
+    }
+    const isAdmin = await verifyUserAdmin(accessToken, orgId);
+    if (!isAdmin) {
+      return { success: false, error: new Error("User must be an admin.") };
+    }
     const currentPollId = uuid();
     await Promise.all([
       db.ref(`polls/${currentPollId}`).set([]),
@@ -89,7 +152,16 @@ export const stopCurrentPoll = functions.https.onCall(
   async (data: StopCurrentPollData) => {
     const { accessToken, userId, orgId } = data;
     const isAllowed = verifyUserId(accessToken, userId);
-    if (!isAllowed) return { success: false };
+    if (!isAllowed) {
+      return {
+        success: false,
+        error: new Error("Access token does not match user id."),
+      };
+    }
+    const isAdmin = await verifyUserAdmin(accessToken, orgId);
+    if (!isAdmin) {
+      return { success: false, error: new Error("User must be an admin.") };
+    }
     const snapshot = await firestore.collection("admin").doc(orgId).get();
     const { currentPollId } = snapshot.data() as StoredSetup;
     await firestore
@@ -100,5 +172,29 @@ export const stopCurrentPoll = functions.https.onCall(
         previousPollIds: admin.firestore.FieldValue.arrayUnion(currentPollId),
       });
     return { success: true };
+  }
+);
+
+export const latestPollResults = functions.https.onRequest(
+  async (request, response) => {
+    const orgId = request.query?.orgId;
+    if (typeof orgId !== "string") response.redirect("/404.html");
+    const snapshot = await firestore
+      .collection("admin")
+      .doc(orgId as string)
+      .get();
+    if (!snapshot.exists) response.redirect("/404.html");
+    const setup = snapshot.data() as StoredSetup;
+    if (typeof setup.currentPollId === "string") {
+      response.redirect(`/pollResults?pollId=${setup.currentPollId}`);
+    } else if (setup.previousPollIds?.length > 0) {
+      response.redirect(
+        `/pollResults?pollId=${
+          setup.previousPollIds[setup.previousPollIds.length - 1]
+        }`
+      );
+    } else {
+      response.redirect("/404.html");
+    }
   }
 );
