88fdaec4e82e934cbf8750d5fd6e22967a1660e3
timmedia <contact@tim-media.com>
2023-04-19 23:09:03 +0200

    feat: added `createPollLayout` function

---
+++ functions/src/create-poll-layout.ts
@@ -0,0 +1,39 @@
+import * as functions from "firebase-functions";
+import { v4 as uuidv4 } from "uuid";
+import { z } from "zod";
+import { verifyAccessToken, verifyUserRole } from "./utils";
+import { firestore } from "./admin";
+
+const CreatePollLayoutData = z.object({
+  accessToken: z.string(),
+  areas: z
+    .array(
+      z.object({
+        x: z.number().gte(0).lte(1),
+        y: z.number().gte(0).lte(1),
+        width: z.number().gte(0).lte(1),
+        height: z.number().gte(0).lte(1),
+      })
+    )
+    .min(1),
+});
+
+export default functions.https.onCall(async (data) => {
+  try {
+    const { accessToken, areas } = CreatePollLayoutData.parse(data);
+    const { orgId } = verifyAccessToken(accessToken);
+    const isAdmin = await verifyUserRole(accessToken, "Admin");
+    if (!isAdmin) throw new Error("User must be admin of org.");
+    const layoutId = uuidv4();
+    await firestore
+      .collection("admin")
+      .doc(orgId)
+      .collection("layouts")
+      .doc(layoutId)
+      .update({ areas });
+    return { success: true, layoutId };
+  } catch (error) {
+    console.log(error);
+    return { success: false, error };
+  }
+});
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,10 +6,12 @@ import createPoll from "./create-poll";
 import latestPollResults from "./latest-poll-results";
 import stopCurrentPoll from "./stop-current-poll";
 import submitVote from "./submit-vote";
+import createPollLayout from "./create-poll-layout";
 
 export const screenPoll = {
   createPoll,
   latestPollResults,
   stopCurrentPoll,
   submitVote,
+  createPollLayout,
 };
