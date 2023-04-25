36c753ccd399c4ab3bb8b441b50adeca2012b3b6
timmedia <contact@tim-media.com>
2023-04-25 22:44:10 +0200

    build: clean up dependencies and set environments

--- admin-embed/vite.config.ts
+++ admin-embed/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import packageJson from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5174,
+  },
+  envDir: "../",
+  define: {
+    "import.meta.env.VITE_APP_VERSION": JSON.stringify(
+      process.env.npm_package_version
+    ),
   },
 });
