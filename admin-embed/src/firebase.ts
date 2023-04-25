82a86f023bc88570680979c9d2c7adc1ea72c962
timmedia <contact@tim-media.com>
2023-04-25 22:50:20 +0200

    refactor: match firebase function definitions

--- admin-embed/src/firebase.ts
+++ admin-embed/src/firebase.ts
@@ -1,6 +1,18 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getFunctions } from "firebase/functions";
+import {
+  DocumentData,
+  collection,
+  doc,
+  getFirestore,
+  onSnapshot,
+} from "firebase/firestore";
+import { getFunctions, httpsCallable } from "firebase/functions";
+import type {
+  CreatePollData,
+  StopPollData,
+  SavePollLayoutData,
+  DeletePollLayoutData,
+} from "./schemas";
 
 const app = initializeApp({
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
@@ -8,5 +20,67 @@ const app = initializeApp({
   databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
 });
 
-export const firestore = getFirestore(app);
-export const functions = getFunctions(app);
+const firestore = getFirestore(app);
+const functions = getFunctions(app);
+
+export const hostingBaseURL = `https://${
+  import.meta.env.VITE_FIREBASE_PROJECT_ID
+}.firebaseapp.com`;
+
+export async function createPoll(data: CreatePollData) {
+  const func = httpsCallable<
+    CreatePollData,
+    { success: boolean; pollId?: string; error?: Error }
+  >(functions, "screenPoll-createPoll");
+  const result = await func(data);
+  return result.data;
+}
+
+export async function stopCurrentPoll(accessToken: string) {
+  const func = httpsCallable<StopPollData, { success: boolean; error?: Error }>(
+    functions,
+    "screenPoll-stopCurrentPoll"
+  );
+  const result = await func({ accessToken });
+  return result.data;
+}
+
+export function savePollLayout(data: SavePollLayoutData) {
+  const func = httpsCallable<
+    SavePollLayoutData,
+    { success: boolean; error?: Error }
+  >(functions, "screenPoll-savePollLayout");
+  return func(data);
+}
+
+export async function deletePollLayout(data: DeletePollLayoutData) {
+  const func = httpsCallable<
+    DeletePollLayoutData,
+    { success: boolean; error?: Error }
+  >(functions, "screenPoll-deletePollLayout");
+  const result = await func(data);
+  return result.data;
+}
+
+// listen to changes in document
+export const onDocSnapshot = (
+  collectionPath: string,
+  documentName: string,
+  callback: (data: DocumentData) => void
+) => {
+  const ref = doc(collection(firestore, collectionPath), documentName);
+  return onSnapshot(ref, (doc) => {
+    if (doc.exists()) callback(doc.data());
+  });
+};
+
+// listen to changes of all document in collection
+export const onCollSnapshot = (
+  collectionPath: string,
+  callback: (data: DocumentData[]) => void
+) => {
+  const ref = collection(firestore, collectionPath);
+  return onSnapshot(ref, (data) => {
+    callback(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  });
+};
--- admin-embed/src/schemas.ts
+++ admin-embed/src/schemas.ts
@@ -1,14 +1,59 @@
 import { z } from "zod";
 
-export const CreatePollData = z.object({
-  accessToken: z.string(),
+const PollLayout = z.object({
+  name: z.string(),
+  areas: z
+    .array(
+      z.object({
+        x: z.number().gte(0).lte(1),
+        y: z.number().gte(0).lte(1),
+        width: z.number().gte(0).lte(1),
+        height: z.number().gte(0).lte(1),
+      })
+    )
+    .min(1),
 });
+
+export type PollLayout = z.infer<typeof PollLayout>;
+
+const CreatePollData = z.object({
+  accessToken: z.string(),
+  layout: PollLayout.nullable(),
+});
+
+export type CreatePollData = z.infer<typeof CreatePollData>;
 
 export const StoredSetupSchema = z.object({
   pollId: z.string().uuid().nullable(),
-  previousPollId: z.array(z.string().uuid()),
 });
 
 export const StopPollData = z.object({
   accessToken: z.string(),
 });
+
+const SavePollLayoutData = z.object({
+  accessToken: z.string(),
+  name: z.string(),
+  id: z.string().uuid(),
+  areas: z
+    .array(
+      z.object({
+        x: z.number().gte(0).lte(1),
+        y: z.number().gte(0).lte(1),
+        width: z.number().gte(0).lte(1),
+        height: z.number().gte(0).lte(1),
+      })
+    )
+    .min(1),
+});
+
+export const DeletePollLayoutData = z.object({
+  accessToken: z.string(),
+  layoutId: z.string().uuid(),
+});
+
+export type DeletePollLayoutData = z.infer<typeof DeletePollLayoutData>;
+
+export type SavePollLayoutData = z.infer<typeof SavePollLayoutData>;
+
+export type StopPollData = z.infer<typeof StopPollData>;
